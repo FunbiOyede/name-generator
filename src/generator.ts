a5b6abef96b16401d8affe9d04752365fc59f93d
Oyede Oluwafunbi <oyedetobi49@gmail.com>
2020-05-17 07:22:06 +0100

    concat string

--- src/generator.ts
+++ src/generator.ts
@@ -4,7 +4,7 @@ export default class Generator{
     generate(): string{
         const randomeAdjective = Adjectives[Math.floor(Math.random() * Adjectives.length)];
         const randomNoun = Nouns[Math.floor(Math.random() * Nouns.length)];
-        const randomName = randomeAdjective + randomNoun;
+        const randomName = randomeAdjective + " " +randomNoun;
         return randomName
 
     }
